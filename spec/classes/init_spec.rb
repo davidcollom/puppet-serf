92de417460e3bfe52a49548c948c3c91bfd362f6
David Collom <david@collom.co.uk>
2014-04-01 16:29:13 +0100

    Fix some outstanding tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 describe 'serf' do
   it { should compile }
+  it { should compile.with_all_deps }
   it { should contain_class('serf::install') }
   it { should contain_class('serf::config') }
   it { should contain_class('serf::service') }
 
   context 'relationship order' do
     it { should contain_class('serf::install').that_comes_before('Class[serf::config]') }
-    xit { should contain_class('serf::config').that_comes_before('Class[serf::service]') }
-    xit { should contain_class('serf::service').that_comes_after('Class[service::config]') }
+    it { should contain_class('serf::config').that_requires('Class[serf::install]') }
+    it { should contain_class('serf::config').that_notifies('Class[serf::service]')}
   end
 
   context 'on Unsupported OS' do
